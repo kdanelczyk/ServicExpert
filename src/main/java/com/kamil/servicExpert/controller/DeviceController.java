b3aab43525807353ca326a65e6093f909c847efa
Kamil <dunkenblatjoseph@gmail.com>
2022-10-20 20:51:47 +0200

    Add controllers with endpoints

---
+++ src/main/java/com/kamil/servicExpert/controller/DeviceController.java
@@ -0,0 +1,210 @@
+package com.kamil.servicExpert.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kamil.servicExpert.db.mapper.DeviceMapper;
+import com.kamil.servicExpert.db.model.Device;
+import com.kamil.servicExpert.model.Device.DeviceGet;
+import com.kamil.servicExpert.model.Device.DeviceGetDetails;
+import com.kamil.servicExpert.model.Device.DevicePost;
+import com.kamil.servicExpert.service.DeviceService;
+
+@CrossOrigin(origins = "http://localhost:8081")
+@RestController
+@RequestMapping("/api")
+public class DeviceController {
+
+	@Autowired
+	private DeviceService deviceService;
+	
+	@Autowired
+	private DeviceMapper deviceMapper;
+
+	@GetMapping("/devices/repaired")
+	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+	public ResponseEntity<CollectionModel<DeviceGet>> findByRepaired() {
+		if (deviceService.findByRepaired(true).isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+		return ResponseEntity.ok(CollectionModel.of(deviceService.findByRepaired(true)
+				.stream()
+				.map(deviceMapper::deviceToDeviceGet)
+				.collect(Collectors.toList()),
+				WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+						.methodOn(this.getClass())
+						.findByRepaired())
+						.withSelfRel(),
+				WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+						.methodOn(this.getClass())
+						.findByNotRepaired())
+						.withRel("not-repaired-devices"),
+				WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+						.methodOn(this.getClass())
+						.getAllDevices())
+						.withRel("all-devices")));
+	}
+
+	@GetMapping("/devices/not-repaired")
+	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+	public ResponseEntity<CollectionModel<DeviceGet>> findByNotRepaired() {
+		if (deviceService.findByRepaired(false).isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+		return ResponseEntity.ok(CollectionModel.of(deviceService.findByRepaired(false)
+				.stream()
+				.map(deviceMapper::deviceToDeviceGet)
+				.collect(Collectors.toList()),
+				WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+						.methodOn(this.getClass())
+						.findByNotRepaired())
+						.withSelfRel(),
+				WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+						.methodOn(this.getClass())
+						.findByRepaired())
+						.withRel("not-repaired-devices"),
+				WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+						.methodOn(this.getClass())
+						.getAllDevices())
+						.withRel("all-devices")));
+	}
+
+	@GetMapping("/devices")
+	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+	public ResponseEntity<CollectionModel<DeviceGet>> getAllDevices() {
+		if (deviceService.findAll().isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+		return ResponseEntity.ok(CollectionModel.of(deviceService.findAll()
+				.stream()
+				.map(deviceMapper::deviceToDeviceGet)
+				.collect(Collectors.toList()),
+				WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+						.methodOn(this.getClass())
+						.getAllDevices())
+						.withSelfRel(),
+				WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+						.methodOn(this.getClass())
+						.findByRepaired())
+						.withRel("repaired-devices"),
+				WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+						.methodOn(this.getClass())
+						.findByNotRepaired())
+						.withRel("not-repaired-devices"),
+				WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+						.methodOn(this.getClass())
+						.deleteAllDevices())
+						.withRel("delete-devices")));
+	}
+
+	@GetMapping("/devices/{id}")
+	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+	public ResponseEntity<EntityModel<DeviceGetDetails>> getDeviceById(@PathVariable("id") long id) {
+		if (deviceService.findById(id).isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		return ResponseEntity.ok(EntityModel.of(deviceMapper.deviceToDeviceGetDetails(deviceService.findById(id).get()),
+				WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+						.methodOn(this.getClass())
+						.getDeviceById(id))
+						.withSelfRel(),
+				WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+						.methodOn(this.getClass())
+						.getAllDevices())
+						.withRel("all-devices"),
+				WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+						.methodOn(RepairController.class)
+						.getAllRepairsByDeviceId(id))
+						.withRel("all-repairs-by-device-id"),
+				WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+						.methodOn(this.getClass())
+						.updateDevice(id, deviceService.findById(id).get()))
+						.withRel("update-device"),
+				WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+						.methodOn(this.getClass())
+						.deleteDevice(id))
+						.withRel("delete-device"),
+				WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+						.methodOn(RepairController.class)
+						.deleteAllRepairsOfDevices(id))
+						.withRel("delete-all-repairs-of-device")));
+	}
+
+	@GetMapping("/types/{typeId}/devices")
+	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+	public ResponseEntity<CollectionModel<DeviceGet>> getAllDevicesByTypeId(@PathVariable(value = "typeId") Long typeId) {
+		if (deviceService.findByTypeId(typeId).isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+		return ResponseEntity.ok(CollectionModel.of(deviceService.findByTypeId(typeId)
+				.stream()
+				.map(deviceMapper::deviceToDeviceGet)
+				.collect(Collectors.toList()),
+				WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+						.methodOn(this.getClass())
+						.getAllDevicesByTypeId(typeId))
+						.withSelfRel(),
+				WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+						.methodOn(TypeController.class)
+						.getTypeById(typeId))
+						.withRel("type-id"),
+				WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+						.methodOn(this.getClass())
+						.deleteAllDevicesOfType(typeId))
+						.withRel("delete-devices-by-type-id")));
+	}
+
+	@PostMapping("/types/{typeId}/devices")
+	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+	public ResponseEntity<Device> createDeviceForType(@PathVariable(value = "typeId") Long typeId,
+			@RequestBody DevicePost deviceRequest) {
+		return new ResponseEntity<>(deviceService.createDeviceForType(typeId, deviceMapper.deviceInputToDevice(deviceRequest)), HttpStatus.CREATED);
+	}
+
+	@PutMapping("/devices/{id}")
+	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+	public ResponseEntity<Device> updateDevice(@PathVariable("id") long id, @RequestBody Device deviceRequest) {
+		if (deviceService.findById(id).isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<>(deviceService.updateDevice(id, deviceRequest), HttpStatus.OK);
+	}
+
+	@DeleteMapping("/devices")
+	@PreAuthorize("hasRole('MODERATOR') or hasRole('ADMIN')")
+	public ResponseEntity<HttpStatus> deleteAllDevices() {
+		deviceService.deleteAll();
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+
+	@DeleteMapping("/devices/{id}")
+	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+	public ResponseEntity<HttpStatus> deleteDevice(@PathVariable("id") long id) {
+		deviceService.deleteById(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+
+	@DeleteMapping("/types/{typeId}/devices")
+	@PreAuthorize("hasRole('MODERATOR') or hasRole('ADMIN')")
+	public ResponseEntity<List<Device>> deleteAllDevicesOfType(@PathVariable(value = "typeId") Long typeId) {
+		deviceService.deleteByTypeId(typeId);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+}
---
+++ src/main/java/com/kamil/servicExpert/controller/NoteController.java
@@ -0,0 +1,152 @@
+package com.kamil.servicExpert.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kamil.servicExpert.db.mapper.NoteMapper;
+import com.kamil.servicExpert.db.model.Note;
+import com.kamil.servicExpert.model.Note.NoteGet;
+import com.kamil.servicExpert.model.Note.NotePost;
+import com.kamil.servicExpert.service.NoteService;
+
+@CrossOrigin(origins = "http://localhost:8081")
+@RestController
+@RequestMapping("/api")
+public class NoteController {
+	
+
+	@Autowired
+	private NoteService noteService;
+	
+	@Autowired
+	private NoteMapper noteMapper;
+	
+	@GetMapping("/notes")
+	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+	public ResponseEntity<CollectionModel<NoteGet>> getAllNotes() {
+		if (noteService.findAll().isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+		return ResponseEntity.ok(CollectionModel.of(noteService.findAll()
+				.stream()
+				.map(noteMapper::noteToNoteGet)
+				.collect(Collectors.toList()),
+				WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+						.methodOn(this.getClass())
+						.getAllNotes())
+						.withSelfRel(),
+				WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+						.methodOn(this.getClass())
+						.deleteAllNotes())
+						.withRel("delete-notes")));
+	}
+	
+	@GetMapping("/notes/{id}")
+	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+	public ResponseEntity<EntityModel<NoteGet>> getNoteById(@PathVariable("id") long id) {
+		if (noteService.findById(id).isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		return ResponseEntity.ok(EntityModel.of(noteMapper.noteToNoteGet(noteService.findById(id).get()),
+				WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+						.methodOn(this.getClass())
+						.getNoteById(id))
+						.withSelfRel(),
+				WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+						.methodOn(this.getClass())
+						.getAllNotes())
+						.withRel("all-notes"),
+				WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+						.methodOn(this.getClass())
+						.updateNote(id, noteService.findById(id).get()))
+						.withRel("update-note"),
+				WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+						.methodOn(this.getClass())
+						.deleteNote(id))
+						.withRel("delete-note")));
+	}
+
+	@GetMapping("/users/{userId}/notes")
+	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+	public ResponseEntity<CollectionModel<NoteGet>> getAllNotesByUserId(@PathVariable(value = "userId") Long userId) {
+		if (noteService.findByUserId(userId).isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+		return ResponseEntity.ok(CollectionModel.of(noteService.findByUserId(userId)
+				.stream()
+				.map(noteMapper::noteToNoteGet)
+				.collect(Collectors.toList()),
+				WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+						.methodOn(this.getClass())
+						.getAllNotesByUserId(userId))
+						.withSelfRel(),
+				WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+						.methodOn(UserController.class)
+						.getUserById(userId))
+						.withRel("user-by-id"),
+				WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+						.methodOn(this.getClass())
+						.deleteAllNotesByUserId(userId))
+						.withRel("delete-devices-by-type-id")));
+	}
+
+	@PostMapping("/users/{userId}/notes")
+	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+	public ResponseEntity<Note> createNoteForUser(@PathVariable(value = "userId") Long userId,
+			@RequestBody NotePost noteRequest) {
+		return new ResponseEntity<>(noteService.createNoteForUser(userId, noteMapper.noteInputToNote(noteRequest)),HttpStatus.CREATED);
+	}
+	
+	@PostMapping("/notes")
+	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+	public ResponseEntity<Note> createNote(@RequestBody NotePost noteRequest) {
+		return new ResponseEntity<>(noteService.createNote(noteMapper.noteInputToNote(noteRequest)), HttpStatus.CREATED);
+	}
+
+	@PutMapping("/notes/{id}")
+	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+	public ResponseEntity<Note> updateNote(@PathVariable("id") long id, @RequestBody Note noteRequest) {
+		if (noteService.findById(id).isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<>(noteService.updateNote(id, noteRequest), HttpStatus.OK);
+	}
+
+	@DeleteMapping("/notes/{id}")
+	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+	public ResponseEntity<HttpStatus> deleteNote(@PathVariable("id") long id) {
+		noteService.deleteById(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+
+	@DeleteMapping("/users/{userId}/notes")
+	@PreAuthorize("hasRole('MODERATOR') or hasRole('ADMIN')")
+	public ResponseEntity<List<Note>> deleteAllNotesByUserId(@PathVariable(value = "userId") Long userId) {
+		noteService.deleteByUserId(userId);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+
+	@DeleteMapping("/notes")
+	@PreAuthorize("hasRole('MODERATOR') or hasRole('ADMIN')")
+	public ResponseEntity<HttpStatus> deleteAllNotes() {
+		noteService.deleteAll();
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+}
