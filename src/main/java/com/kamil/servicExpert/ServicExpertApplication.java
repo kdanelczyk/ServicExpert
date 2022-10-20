c8d3392bac058dd6f123ebd4f20e21a309c19e85
Kamil <dunkenblatjoseph@gmail.com>
2022-10-20 19:25:51 +0200

    Init

---
+++ src/main/java/com/kamil/servicExpert/ServicExpertApplication.java
@@ -0,0 +1,13 @@
+package com.kamil.servicExpert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicExpertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicExpertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kamil/servicExpert/ServicExpertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamil.servicExpert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicExpertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
